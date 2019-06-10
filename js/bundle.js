19829abea8c24c9fa13928b268aea783b9ec128c
unknown <okumura6883@gmail.com>
2019-06-10 11:56:34 -0700

    AutoUpdate

--- js/bundle.js
+++ js/bundle.js
@@ -52773,8 +52773,6 @@ module.exports = class Game {
                     this.generateMap();
                     this.gameloop = setInterval(() => this.tick(), gameTick);
                     Swal.close();
-                    window.game = this;
-                    console.log("Done Init");
                 }, 500);
             }
         });
@@ -53217,7 +53215,10 @@ module.exports = class Game {
             imageUrl: 'https://i.imgur.com/HTIBIQv.png',
             imageWidth: 600,
             width: 700,
-            html: '<div class="fb-share-button" data-href="https://ucsdgame.dev/earth" data-layout="button" data-size="small"><a target="_blank" href="https://www.facebook.com/sharer/sharer.php?u=https%3A%2F%2Fucsdgame.dev%2Fearth&amp;src=sdkpreparse" class="fb-xfbml-parse-ignore">Share</a></div>',
+            html: `<div class="fb-share-button" 
+            data-href="https://ucsdgame.dev/earth" 
+            data-layout="button_count">
+          </div>`,
             showCancelButton: true,
             cancelButtonColor: "#26a4ff",
             cancelButtonText: "Play Again",
